a165fd870f0fe0c21cf474ddc37d0101eccf17e4
Radek <balicki56@gmail.com>
2026-01-03 01:56:55 +0000

    Init

---
+++ converter/components/ConvertButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/button.css";
+const ConvertButton = () => {
+  return (
+    <div>
+      <button className="button">Convert</button>
+    </div>
+  );
+};
+
+export default ConvertButton;
